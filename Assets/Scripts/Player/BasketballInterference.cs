650c031c359de8ba2db76f090d2a2236cfbfe624
dkwapisz <dkwapisz@student.agh.edu.pl>
2022-05-05 21:07:03 +0200

    Refactoring

--- Assets/Scripts/Player/BasketballInterference.cs
+++ Assets/Scripts/Player/BasketballInterference.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Basketball_Interference : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class BasketballInterference : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.CompareTag("Basketball"))
