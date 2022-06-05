1d3ee910a895381c504ba2a40f9cc7121a647bc7
michpil19 <pilchu2501@gmail.com>
2022-06-05 18:48:40 +0200

    Kamera wyciągnięta na wszystkich 3 scenach z prefaba playera - GravityTool działa, animacja do góry nogami pasuje.

---
+++ Assets/Scripts/Player/ReverseGravity.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseGravity : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+   
+    public bool isGravityNormal;
+    
+
+    void Start()
+    {
+        isGravityNormal = true;
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag.Equals("ReverseGravityTool"))
+        {
+            //variable = (condition) ? expressionTrue :  expressionFalse;
+            isGravityNormal = (isGravityNormal) ?  false : true;
+            Reverse();
+            GameObject player = GameObject.Find("Player");
+            CharacterController2D characterController2D = player.GetComponent<CharacterController2D>();
+            characterController2D.jumpForce *= -1;
+            Debug.Log("Gravity Reversed");
+            Destroy(collider.gameObject);
+        }
+    }
+    
+    private void Reverse()
+    {
+        rb2D.gravityScale *= -1;
+        rb2D.transform.localScale = new Vector3(-1, 2, 1);
+        if (isGravityNormal == false){
+            transform.eulerAngles = new Vector3(0, 0, 180f);
+        }else
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+    }
+}
